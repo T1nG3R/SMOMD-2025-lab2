de6bcc3ad7d0853d883f8f31085ef239bbe2f91b
T1nG3R <46874273+T1nG3R@users.noreply.github.com>
2025-03-19 20:43:30 +0200

    Initial commit

---
+++ kafka_lab_consumer/src/main/java/com/example/StarbucksDeserializer.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class StarbucksDeserializer implements Deserializer<StarbucksProduct> {
+    private final Logger logger = LoggerFactory.getLogger(StarbucksDeserializer.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+
+    @Override
+    public StarbucksProduct deserialize(String topic, byte[] data) {
+        try {
+            if (data == null) {
+                return null;
+            }
+            return objectMapper.readValue(data, StarbucksProduct.class);
+        } catch (Exception e) {
+            logger.error("Помилка десеріалізації даних у StarbucksProduct: ", e);
+            return null;
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+}
